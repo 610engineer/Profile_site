9f35c60cf1c17049227000b767467e4f6877ce64
masato muto <m_muto@jour-1.com>
2020-09-22 21:46:49 +0900

    jQuery is active only on front page

--- jour1/footer.php
+++ jour1/footer.php
@@ -11,21 +11,25 @@
 
 
 <?php wp_footer();?>
-<div class = "start">
+
+    
+<?php if(is_front_page()): ?>
+    <div class = "start">
         <p><img src="https://jour-1.com/wp-content/uploads/2020/09/image1.jpeg" alt=""></p>
     </div>
-<script>
-jQuery(function($) {
-	setTimeout(function(){
-    //fadein Logo in 0.1sec
-		$('.start p').fadeIn();
-	},100); 
-	setTimeout(function(){
-    //feeOut white screen in 1sec
-		$('.start').fadeOut();
-	},1000); 
-});
-</script>
+    <script>
+    jQuery(function($) {
+        setTimeout(function(){
+        //fadein Logo in 0.1sec
+            $('.start p').fadeIn();
+        },100); 
+        setTimeout(function(){
+        //feeOut white screen in 1sec
+            $('.start').fadeOut();
+        },1000); 
+    });
+    </script>
+<?php endif; ?>
     
 
 </body>
